9bf8639ce32b2985e19c55290637bd7bd9636f85
abhi967792@gmail.com <abhi967792@gmail.com>
2020-11-18 11:02:57 +0530

    Path error resolved

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
 function allposts(){
-<<<<<<< HEAD
     
     window.location.href="../ScribblerProject/html/bloglist.html";
 }
-=======
-    window.location.href="../html/bloglist.html";
-}
->>>>>>> 80ca9f646b17fd242954d671a1f74c0afb4786e2
